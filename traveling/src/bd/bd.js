a5c5277578e60fb278a2673dc9082ed5208a8bb4
yruiu <46344823+yruiu@users.noreply.github.com>
2022-01-30 19:04:41 +0200

    Delete traveling directory

--- traveling/src/bd/bd.js
+++
@@ -1,8 +0,0 @@
-let bd={
-    "DomiH4": "Домініканська Республіка",
-    "ano1": "Туристична країна в якій на потрібна віза",
-    "seH4": "Сейшельські Острови",
-    "seano":"Курортний острів Сейшельські Острови",
-    "cardseH3":"Республіка Сейшельські Островифр. République des Seychellesангл. Republic of Seychelles сейш. Repiblik Sesel"
-}
-export default bd;
